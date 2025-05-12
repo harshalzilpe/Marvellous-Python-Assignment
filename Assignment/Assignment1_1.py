5e10e1535dea1eda059392b4962161cb84c1a081
Harshal Zilpe <108272214+harshalzilpe@users.noreply.github.com>
2025-05-12 15:56:55 +0530

    Add files via upload

---
+++ Assignment/Assignment1_1.py
@@ -0,0 +1,16 @@
+# Write a program which contains one function name as Fun(). That function should display "Hello from Fun" on Console.
+
+def Fun():
+    print("Hello from Fun")
+
+def main():
+    Fun()
+    
+if __name__ == "__main__":
+    main() 
+    
+""" 
+    macbook@192 Assignment % python3 Assignment1_1.py
+    Hello from Fun
+    macbook@192 Assignment % 
+"""
